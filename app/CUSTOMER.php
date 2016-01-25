b22f2781706a82dce237e10f113bac4cf3468ff9
Nilesh Jayanandana <nilesh.jayanandana@yahoo.com>
2016-01-25 21:52:35 +0530

    Fixed customer Conflict and added proper comments

--- app/CUSTOMER.php
+++ app/CUSTOMER.php
@@ -1,15 +1,19 @@
 <?php
-
 namespace App;
-
 use Illuminate\Database\Eloquent\Model;
-
-class Customer extends Model
+class CUSTOMER extends Model
 {
+    /*
+|--------------------------------------------------------------------------
+| Laravel Default Model Overrides
+|--------------------------------------------------------------------------
+|
+|
+*/
     protected $table = 'CUSTOMER';
-
     protected $primaryKey = 'cus_id';
-
+ 
+    public $timestamps  = false;
     protected $fillable = [
         'name',
         'NIC/passport_num',
@@ -23,6 +27,63 @@ class Customer extends Model
         'zip_code',
         'country'
     ];
-
     public $timestamps = false;
+    
+    
+/*
+|
+|
+|--------------------------------------------------------------------------
+| End Laravel Default Model Overrides
+|--------------------------------------------------------------------------
+*/
+    
+    
+    
+/*
+|--------------------------------------------------------------------------
+| Rishan Model Functions
+|--------------------------------------------------------------------------
+|
+|
+*/
+    
+    public function ROOM_RESERVATION()
+    {
+        return $this->hasMany('App\ROOM_RESERVATION','room_reservation_id');
+  }
 }
+
+
+
+
+
+
+/*
+|
+|
+|--------------------------------------------------------------------------
+| End Rishan Model Functions
+|--------------------------------------------------------------------------
+*/
+
+
+/*
+|--------------------------------------------------------------------------
+| Vishan Model Functions
+|--------------------------------------------------------------------------
+|
+|
+*/
+
+
+
+
+
+/*
+|
+|
+|--------------------------------------------------------------------------
+| End Vishan Model Functions
+|--------------------------------------------------------------------------
+*/
